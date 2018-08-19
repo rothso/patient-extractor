d95ff35b6e8f30906ba6577084c41ba84b6b7cdc
Rothanak So <rothanak.hello@gmail.com>
2018-08-19 00:25:14 -0400

    Minor cleanup + skeleton for the future

--- src/main/kotlin/com/github/rothso/mass/extractor/PatientExtractor.kt
+++ src/main/kotlin/com/github/rothso/mass/extractor/PatientExtractor.kt
@@ -1,6 +1,8 @@
 package com.github.rothso.mass.extractor
 
 import com.github.rothso.mass.extractor.network.NetworkProvider
+import com.github.rothso.mass.extractor.network.athena.response.Patient
+import io.reactivex.schedulers.Schedulers
 
 class PatientExtractor {
   private val athena = NetworkProvider.createAthenaClient()
@@ -14,25 +16,40 @@ class PatientExtractor {
   fun redactSummaries() {
     athena.getAllPatients() // (1)
         .flattenAsFlowable { it.patients }
-        .map { it.patientid }
-        .distinct()
-        .doOnNext { println(it) }
+        .distinct { it.patientid }
         .onBackpressureBuffer()
-        .flatMap({ patientid ->
-          athena.getPatientEncounters(patientid) // (2)
-              .doOnSubscribe { println("Requesting patient encounters for $patientid") }
+        .flatMap({ patient ->
+          val id = patient.patientid
+          athena.getPatientEncounters(id) // (2)
+              .doOnSubscribe { println("Requesting patient encounters for $id") }
               .doOnSuccess { println("Got encounters") }
               .flattenAsFlowable { it.encounters }
               .onBackpressureBuffer()
               .flatMapSingle({ encounter ->
                 athena.getEncounterSummary(encounter.encounterId) // (3)
-                    .doOnSubscribe { println("Requesting summaries for $patientid") }
+                    .doOnSubscribe { println("Requesting summaries for $id") }
                     .doOnSuccess { println("Got summary") }
               }, false, 1)
-              .doOnComplete { println("Finished $patientid") }
-        }, false, 3)
-        .blockingSubscribe({}, Throwable::printStackTrace)
+              .doOnComplete { println("Finished $id") }
+              .map { Pair(patient, it) }
+        }, false, 3) // TODO may need to tweak maxConcurrency in prod build
+        .observeOn(Schedulers.computation())
+        .map { (patient, summary) -> fake(patient, summary.html) }
+        .blockingSubscribe({ html ->
+          saveAsHtml(html)
+          saveAsPdf(html)
+        }, Throwable::printStackTrace)
+  }
 
-    // TODO Faker search/replace; save to .html; open .html in Word to convert to .pdf
+  private fun fake(patient: Patient, html: String): String {
+    TODO()
+  }
+
+  private fun saveAsHtml(html: String) {
+    TODO()
+  }
+
+  private fun saveAsPdf(html: String) {
+    TODO()
   }
 }
